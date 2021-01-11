943b898884a5ccf39d3b77c79d5d81a647d578f9
kxxt <18085551+kxxt@users.noreply.github.com>
2021-01-11 08:38:23 +0800

    Finish basic funcinality.

--- AutoSdu/AutoSdu/App.xaml.cs
+++ AutoSdu/AutoSdu/App.xaml.cs
@@ -1,20 +1,48 @@
 ﻿using AutoSdu.Views;
 using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace AutoSdu
 {
 	public partial class App : Application
 	{
-
+		public string DataDir { get; private set; }
+		public string ConfigFilePath { get; private set; }
 		public App()
 		{
 			InitializeComponent();
 
 			MainPage = new AppShell();
+			DataDir = FileSystem.AppDataDirectory;
+			ConfigFilePath = DataDir +Path.DirectorySeparatorChar+"config";
 		}
 
+		public (string,string) ReadConfig()
+		{
+			if (!File.Exists(ConfigFilePath))
+			{
+				File.Create(ConfigFilePath).Close();
+				return ("", "");
+			}
+			var lines=File.ReadAllLines(ConfigFilePath);
+			try
+			{
+				return (lines[0], lines[1]);
+			}
+			catch
+			{
+				SaveConfig("","");
+				return ("", "");
+			}
+		}
+
+		public void SaveConfig(string url, string username)
+		{
+			File.WriteAllText(ConfigFilePath,url+'\n'+username);
+		}
 		protected override void OnStart()
 		{
 		}
--- AutoSdu/AutoSdu/Views/MainPage.xaml.cs
+++ AutoSdu/AutoSdu/Views/MainPage.xaml.cs
@@ -1,14 +1,21 @@
-﻿using AutoSdu.Models;
+﻿using Android.Provider;
+using Android.Media;
+using AutoSdu.Models;
 using AutoSdu.ViewModels;
 using AutoSdu.Views;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
 
 namespace AutoSdu.Views
 {
@@ -16,10 +23,10 @@ namespace AutoSdu.Views
 	{
 		ItemsViewModel _viewModel;
 
+		public const string ImagePath = "/mnt/sdcard/AutoSdu/data.png";
 		public MainPage()
 		{
 			InitializeComponent();
-
 			BindingContext = _viewModel = new ItemsViewModel();
 		}
 
@@ -27,6 +34,84 @@ namespace AutoSdu.Views
 		{
 			base.OnAppearing();
 			_viewModel.OnAppearing();
+			LoadConfig();
+			Debug.WriteLine((App.Current as App).ConfigFilePath);
+		}
+
+		private void LoadConfig()
+		{
+			var cfg = (App.Current as App).ReadConfig();
+			URL.Text = cfg.Item1;
+			Username.Text = cfg.Item2;
+		}
+
+		private void RetrieveScreenShot(object sender, EventArgs e)
+		{
+			var src = new UriImageSource();
+			src.Uri = new Uri(URL.Text + (URL.Text.EndsWith("/") ? "" : "/") + Username.Text + ".png");
+			//Debug.WriteLine(src.Uri.AbsolutePath);
+			Image.Source = src;
+		}
+
+		private async void SaveToDCIM(object sender, EventArgs e)
+		{
+			WebClient wc = new WebClient();
+			if (!Directory.Exists("/mnt/sdcard/AutoSdu")) Directory.CreateDirectory("/mnt/sdcard/AutoSdu");
+			bool success = false;
+			for (int i = 0; i <= 5; i++)
+			{
+				try
+				{
+					await wc.DownloadFileTaskAsync(new Uri(URL.Text + (URL.Text.EndsWith("/") ? "" : "/") + Username.Text + ".png"), 
+						Path.Combine((App.Current as App).DataDir, ImagePath));
+					success = true;
+					break;
+				}
+
+				catch(Exception ex)
+				{
+					
+				}
+			}
+
+			if (!success)
+			{
+				await DisplayAlert("获取并保存图像失败", "在获取图像或保存到本地时出错\r\n请检查程序是否具有存储权限且当前网络连接良好.\r\n请检查URL和username是否正确.","确定");
+				if (sender == null) await DisplayAlert("提示", "因下载失败, 将使用上次下载的截图", "确定");
+			}
+			else
+			{
+				var s = new FileImageSource();
+				s.File = ImagePath;
+				Image.Source = s;
+				if (sender!=null)
+					await DisplayAlert("保存成功", "已保存到AutoSdu/data.png","确定");
+			}
+		}
+
+		private async void Share(object sender, EventArgs e)
+		{
+			SaveToDCIM(null,null);
+			if (!File.Exists(ImagePath)) return;
+			await Xamarin.Essentials.Share.RequestAsync(new ShareFileRequest
+			{
+				Title = Title,
+				File = new ShareFile(ImagePath)
+			});
+		}
+
+		private void UpdateConfig()
+		{
+			(App.Current as App).SaveConfig(URL.Text, Username.Text);
+		}
+		private void URL_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			UpdateConfig();
+		}
+
+		private void Username_OnTextChanged(object sender, TextChangedEventArgs e)
+		{
+			UpdateConfig();
 		}
 	}
 }
